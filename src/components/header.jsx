d9e595b25f63ad980c50687595d8c9b2026a660a
TaiwoSoftware <davcodes24@gmail.com>
2023-07-26 23:27:55 +0100

    practicing my react components

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+
 function Header() {
-    return <header>
-        <img src="../images/logo.svg" alt="logo" />
-        <ul>
-            <li>About</li>
-            <li>Discover</li>
-            <li>Get Started</li>
-        </ul>
-        </header>
-    }
+  return (
+    <header>
+      <img src="./images/logo.svg" alt="logo" />
+      <ul>
+        <li>About</li>
+        <li>Discover</li>
+        <li>Get Started</li>
+      </ul>
+    </header>
+  );
+}
 
 export default Header;
